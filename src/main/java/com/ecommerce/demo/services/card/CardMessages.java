97d84d27b2d66c93e24f546e5f4318b3a090b70f
andree.anchi <hairton.anchi.d@uni.pe>
2023-10-01 23:59:20 -0500

    chore: change invalid pan message

--- src/main/java/com/ecommerce/demo/services/card/CardMessages.java
+++ src/main/java/com/ecommerce/demo/services/card/CardMessages.java
@@ -1,7 +1,7 @@
 package com.ecommerce.demo.services.card;
 
 public interface CardMessages {
-    public static final String PAN_INVALID = "Invalid PAN";
+    public static final String PAN_INVALID = "Invalid Card Number";
     public static final String CVV_INVALID = "Invalid CVV";
     public static final String EXPIRATION_DATE_INVALID = "Invalid expiration date";
 }
